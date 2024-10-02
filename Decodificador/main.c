f0b8cb3eafe937f2a083620d2e199f86fc5faff4
Cauê Carneiro <160974341+cauecarneiro@users.noreply.github.com>
2024-10-02 14:09:15 -0300

    Update main.c
    Comentando o código

--- Decodificador/main.c
+++ Decodificador/main.c
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 
 int main(){
-	char mensagem[101], hex[3], fim[] = "00";
+	char mensagem[101], hex[3], fim[] = "00"; // Variáveis para mensagem, conversão e fim.
 	int j,i, b, quantidade, verificar, x = 1;
 
-	
+	// Solicita a quantidade de mensagens ao usuário, garantindo que seja válida.
 	do{
 		printf("\nQuantidade de mensagem: ");
 		scanf("%d", &quantidade);
@@ -16,27 +16,29 @@ int main(){
 		}
 	}while(quantidade <= 0);
 	
+	// Processa cada mensagem.
 	for(j = 1; j <= quantidade; j++){
 		printf("\nColoque o valor de B: ");
 		scanf("%d", &b);
 		printf("Coloque a mensagem: ");
 		scanf("%100s", mensagem);
 		printf("\n\"");
+		 // Processa cada par de caracteres da mensagem.
 		for(i=0; i<strlen(mensagem); i+= 2){
 			x = i / 2 + 1;
-			verificar = func_val(x, b);
+			verificar = func_val(x, b); // Chama função para verificação.
 			if(verificar != 0){
 				hex[0] = mensagem[i];
 				hex[1] = mensagem [i + 1];
 				hex[2] = '\0';
 				if (strcmp(hex, fim) == 0){
-					break;
+					break; // Interrompe se encontrar "00".
 				}
-				int resultado = strtol(hex, NULL, 16);
-				printf("%c", resultado);
+				int resultado = strtol(hex, NULL, 16);  // Converte de hexadecimal para decimal.
+				printf("%c", resultado); // Imprime o caractere resultante.
 			}
 		}
-		printf("\"\n");
+		printf("\"\n"); // Finaliza a impressão da mensagem
 	}
 	return 0;
 }
